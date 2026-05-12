200b1e5b52b0ceb0c9437433066b6e7a197c5fca
Ramonprojects <adm@ramonmago.com>
2026-05-12 19:19:59 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
   export default function handler(req, res) {                                                                                                                                              
-    res.writeHead(302, { Location: 'https://rso.sugestao.cc/' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=NF4J2KXLUFC&afp=so' });
     res.end();                                                                                                                                                                             
   }
