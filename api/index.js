9480bc9e118366b4b7bceebb8c4e180813f7902a
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-22 10:27:58 -0300

    chore: update shareCode to DHE2JOKZD3C

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=I7BJC9EZ3WY&afp=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=DHE2JOKZD3C&afp=so' });
     res.end();
   }
