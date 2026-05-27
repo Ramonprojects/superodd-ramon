9ed9072dee973a520bf9e8e19f14e82b05426d7b
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-27 12:35:32 -0300

    chore: update shareCode to N3L1HO3OGOR

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=WKMQVWZNFTZ&afp=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=N3L1HO3OGOR&afp=so' });
     res.end();
   }
