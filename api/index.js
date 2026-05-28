6009e68ab31828ea9681fb7f9e010ea734256288
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-28 12:55:28 -0300

    chore: update shareCode to 9CXA3RGR4DI

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=N3L1HO3OGOR&afp=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=9CXA3RGR4DI&afp=so' });
     res.end();
   }
