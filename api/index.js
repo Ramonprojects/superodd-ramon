d32fa42b25a25b643ca99acb4b2eaf80a57ca0b1
Ramonprojects <adm@ramonmago.com>
2026-05-29 14:22:22 -0300

    chore: shareCode KQCYY8DJCH5 -> QM37GAS1NGI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=KQCYY8DJCH5&afp=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=QM37GAS1NGI&afp=so' });
     res.end();
   }
