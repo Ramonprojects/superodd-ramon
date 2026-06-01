76d6ac8dbf03f4239b2010ced55f552e654a5735
Ramonprojects <adm@ramonmago.com>
2026-06-01 10:02:21 -0300

    chore: shareCode T18A8Q9D7VR -> WUFV6WPZUFR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=T18A8Q9D7VR&afp=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=WUFV6WPZUFR&afp=so' });
     res.end();
   }
