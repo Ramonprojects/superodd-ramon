bd88a7d37c3191509c036cbdf0fd13b5925b6f9b
Ramonprojects <adm@ramonmago.com>
2026-06-02 09:47:25 -0300

    chore: shareCode WUFV6WPZUFR -> M5W3RKS4GZ9 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=WUFV6WPZUFR&afp=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=M5W3RKS4GZ9&afp=so' });
     res.end();
   }
