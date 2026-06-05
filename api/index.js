72a27116e1564d4c17bcd850d2675c9576d7d0f7
Ramonprojects <adm@ramonmago.com>
2026-06-05 09:59:34 -0300

    chore: shareCode ZF6HVI3UQ1B -> ROI20RQZBHH (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=ZF6HVI3UQ1B&afp=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=ROI20RQZBHH&afp=so' });
     res.end();
   }
