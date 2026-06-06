ad6388635ae15fcc33e27fdfa6dadf98a566bfe5
Ramonprojects <adm@ramonmago.com>
2026-06-06 12:44:11 -0300

    chore: shareCode ROI20RQZBHH -> 2S86VU20M91 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=ROI20RQZBHH&afp=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=2S86VU20M91&afp=so' });
     res.end();
   }
