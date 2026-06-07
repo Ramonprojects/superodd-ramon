f386672f6616439609df6a80ae19dd56553dfee1
Ramonprojects <adm@ramonmago.com>
2026-06-07 11:34:27 -0300

    chore: shareCode 2S86VU20M91 -> 3XCU9UL2Q78 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=2S86VU20M91&afp=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=3XCU9UL2Q78&afp=so' });
     res.end();
   }
