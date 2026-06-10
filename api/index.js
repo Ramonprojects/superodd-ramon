e1660e1119040ae2c8083de7116b426a286a2814
Ramonprojects <adm@ramonmago.com>
2026-06-10 14:07:51 -0300

    chore: shareCode Q7MZJ88EW61 -> 36D3MIVR1OF (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=Q7MZJ88EW61&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=36D3MIVR1OF&afp1=so' });
     res.end();
   }
