f2706f787e8437748ce324597ae6fedbf888d983
Ramonprojects <adm@ramonmago.com>
2026-06-21 12:37:42 -0300

    chore: shareCode PWH2QJRUVIZ -> HEHQ2R40H9I (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=PWH2QJRUVIZ&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=HEHQ2R40H9I&afp1=so' });
     res.end();
   }
