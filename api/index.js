6c5ee7ed3dfe835f82dac52cd5e4fe142291f88f
Ramonprojects <adm@ramonmago.com>
2026-06-25 12:33:42 -0300

    chore: shareCode S6OLGH7QQBF -> CC26HVM3TIH (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=S6OLGH7QQBF&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=CC26HVM3TIH&afp1=so' });
     res.end();
   }
