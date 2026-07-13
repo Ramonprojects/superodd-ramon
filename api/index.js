d28200b8b0d7aad978ef952b701b87ffb49cbc64
Ramonprojects <adm@ramonmago.com>
2026-07-13 10:51:49 -0300

    chore: shareCode KZ1HC2JKY42 -> YL08UX8WHO1 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=KZ1HC2JKY42&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=YL08UX8WHO1&afp1=so' });
     res.end();
   }
