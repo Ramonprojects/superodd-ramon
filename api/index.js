1b16b8eac4d108a001fde927508ffdd4ddef4485
Ramonprojects <adm@ramonmago.com>
2026-07-14 09:52:11 -0300

    chore: shareCode YL08UX8WHO1 -> FBCLCTJEN25 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=YL08UX8WHO1&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=FBCLCTJEN25&afp1=so' });
     res.end();
   }
