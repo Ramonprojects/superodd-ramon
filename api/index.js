2e80b9803351a1d189c0d7b6b602c08d326f8833
Ramonprojects <adm@ramonmago.com>
2026-07-17 12:22:07 -0300

    chore: shareCode B955KPJVY4Z -> L2APMBOZ20X (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=B955KPJVY4Z&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=L2APMBOZ20X&afp1=so' });
     res.end();
   }
