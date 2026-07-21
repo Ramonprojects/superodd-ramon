063a8ad163bb39e779de998d2c012315893cd217
Ramonprojects <adm@ramonmago.com>
2026-07-21 10:20:24 -0300

    chore: shareCode 7L9ZQE5RWQ2 -> RURL1DZ71NZ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=7L9ZQE5RWQ2&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=RURL1DZ71NZ&afp1=so' });
     res.end();
   }
