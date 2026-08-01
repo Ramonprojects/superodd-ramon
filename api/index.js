d755be2995b352cc1622f6f0de3864f8f6ebb0fc
Ramonprojects <adm@ramonmago.com>
2026-07-31 23:04:11 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=604L2IAW06F&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=604L2IAW06F&afp1=so' });
     res.end();
   }
