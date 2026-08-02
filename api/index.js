ba39ff2dba3ef6ec6b18b51326af477cb2cb7731
Ramonprojects <adm@ramonmago.com>
2026-08-02 11:13:55 -0300

    chore: shareCode WO4S52O4PUI -> SR800IGK9AG (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=WO4S52O4PUI&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=SR800IGK9AG&afp1=so' });
     res.end();
   }
