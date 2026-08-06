322b48f1f3e8cadadf1e6bba5b238d822e0d3f29
Ramonprojects <adm@ramonmago.com>
2026-08-06 10:25:19 -0300

    chore: shareCode SIXS6FPLGI8 -> NOYU093U3TP (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=SIXS6FPLGI8&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=NOYU093U3TP&afp1=so' });
     res.end();
   }
