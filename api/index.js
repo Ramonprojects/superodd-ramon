18ca1720540953c502faba1b194e7028a5d2f390
Ramonprojects <adm@ramonmago.com>
2026-08-08 10:19:13 -0300

    chore: shareCode S9SA87I9Y92 -> E2NMLNPTCJ7 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=S9SA87I9Y92&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=E2NMLNPTCJ7&afp1=so' });
     res.end();
   }
