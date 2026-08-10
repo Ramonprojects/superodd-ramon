060c337b101c59f37073e70f2393b141c1670a0d
Ramonprojects <adm@ramonmago.com>
2026-08-10 10:28:03 -0300

    chore: shareCode 9RR6S0DEGYG -> 33NGNUBQ407 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=9RR6S0DEGYG&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=33NGNUBQ407&afp1=so' });
     res.end();
   }
