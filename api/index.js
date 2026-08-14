d7a050d23826fc4a17a331221ab2f2d5b8e64ec9
Ramonprojects <adm@ramonmago.com>
2026-08-14 10:41:48 -0300

    chore: shareCode FKPFET4Z06E -> 5DRPXJFPMKA (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=FKPFET4Z06E&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=5DRPXJFPMKA&afp1=so' });
     res.end();
   }
