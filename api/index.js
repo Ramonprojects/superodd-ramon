32c0c386cce3ee56d190fd51ff3395873babaa4f
Ramonprojects <adm@ramonmago.com>
2026-08-18 10:33:03 -0300

    chore: shareCode 3KN321U52NW -> Z67U6B3WF5G (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=3KN321U52NW&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=Z67U6B3WF5G&afp1=so' });
     res.end();
   }
