943945c0be7b537df4e696fd2b1397a28bdd616e
Ramonprojects <adm@ramonmago.com>
2026-08-29 12:45:23 -0300

    chore: shareCode 2L3YEECLO2O -> NZFDAUZWP22 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=2L3YEECLO2O&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=NZFDAUZWP22&afp1=so' });
     res.end();
   }
