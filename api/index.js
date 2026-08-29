16872a07f07112f4e863f3ba8170b9e49f8d5cbb
Ramonprojects <adm@ramonmago.com>
2026-08-29 10:04:15 -0300

    chore: shareCode HOQ78SE3A8B -> 2L3YEECLO2O (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=HOQ78SE3A8B&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=2L3YEECLO2O&afp1=so' });
     res.end();
   }
