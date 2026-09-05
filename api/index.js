73032c271f24c52bf20bab42b0c2374421c59dde
Ramonprojects <adm@ramonmago.com>
2026-09-05 08:51:33 -0300

    chore: shareCode ZKT8P14SP6H -> G8UY9ROQSAA (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=ZKT8P14SP6H&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=G8UY9ROQSAA&afp1=so' });
     res.end();
   }
