921cd9dc1f3d7774ce52aad2ba91cc527991590a
Ramonprojects <adm@ramonmago.com>
2026-09-07 10:06:57 -0300

    chore: shareCode OFU60WB3LVH -> 998X72OFXT0 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=OFU60WB3LVH&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=998X72OFXT0&afp1=so' });
     res.end();
   }
