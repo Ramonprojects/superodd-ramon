39817fae6eb36dbe40b6dc256f5eaf2e7b27d4e0
Ramonprojects <adm@ramonmago.com>
2026-09-09 10:19:00 -0300

    chore: shareCode I33WI38ZNNZ -> TNMIVBPFZ8H (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=I33WI38ZNNZ&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=TNMIVBPFZ8H&afp1=so' });
     res.end();
   }
