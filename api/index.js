8cdff800349434f14d6cdb880bc3a3bc0bc974c2
Ramonprojects <adm@ramonmago.com>
2026-09-12 11:25:25 -0300

    chore: shareCode CWMA59UMXUI -> VH8R99M0Q50 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=CWMA59UMXUI&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=VH8R99M0Q50&afp1=so' });
     res.end();
   }
