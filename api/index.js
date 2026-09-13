e54e43873932f4cc9a165f84defd34c3f25ec6bd
Ramonprojects <adm@ramonmago.com>
2026-09-13 08:34:49 -0300

    chore: shareCode VH8R99M0Q50 -> FWYC9PWW83S (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=VH8R99M0Q50&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=FWYC9PWW83S&afp1=so' });
     res.end();
   }
