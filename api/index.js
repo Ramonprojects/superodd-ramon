7aeec260648eef2d74f5eabfbb2748089ae84dca
Ramonprojects <adm@ramonmago.com>
2026-09-14 10:23:16 -0300

    chore: shareCode 90SURIOKYCT -> GDGER3Z39JH (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=90SURIOKYCT&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=GDGER3Z39JH&afp1=so' });
     res.end();
   }
