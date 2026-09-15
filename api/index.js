42745fae3d865d4d7713931864d02f9f4626e3c5
Ramonprojects <adm@ramonmago.com>
2026-09-15 10:48:35 -0300

    chore: shareCode AL9T0JYKJSQ -> PUYDFGRP024 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=AL9T0JYKJSQ&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=PUYDFGRP024&afp1=so' });
     res.end();
   }
