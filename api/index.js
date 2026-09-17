32fa9819052e4bd2ec81a313a1914254343a9cd1
Ramonprojects <adm@ramonmago.com>
2026-09-17 10:29:41 -0300

    chore: shareCode GG5741KVH77 -> LSR8LQS8QF4 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=GG5741KVH77&afp1=so' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=LSR8LQS8QF4&afp1=so' });
     res.end();
   }
